e9baa12dc0a68e941091380088e11db0d1e39262
MishaRocks <tehobox@yandex.ru>
2023-08-30 23:11:07 +0300

    users

--- config/settings.py
+++ config/settings.py
@@ -151,6 +151,6 @@ LOGIN_URL = '/users/'
 
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
-EMAIL_HOST_USER = 'tehobox@ya.ru'
-EMAIL_HOST_PASSWORD = 'vekdnrnogrsfwkwk'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_SSL = True
