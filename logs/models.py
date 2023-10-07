b5367704ab6f464e7780c0e73975870750f0233b
MishaRocks <tehobox@yandex.ru>
2023-10-07 13:23:46 +0300

    users + blog + clients + user + mailsender + logs

--- logs/models.py
+++ logs/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
+
 from mailsender.models import NULLABLE
 
 
 class Logs(models.Model):
-    last_try = models.DateTimeField(verbose_name='Последняя попытка', **NULLABLE)
+    last_try = models.DateTimeField(verbose_name='Последняя попытка', auto_now_add=True, **NULLABLE)
     status = models.CharField(**NULLABLE, verbose_name='Статус')
     server_ans = models.CharField(**NULLABLE, verbose_name='Ответ сервера')
+    mailing = models.ForeignKey('mailsender.Mailing', on_delete=models.CASCADE, **NULLABLE)
+    client = models.ForeignKey('clients.Client', on_delete=models.CASCADE, **NULLABLE)
 
     def __str__(self):
         return f'{self.server_ans}'
--- logs/views.py
+++ logs/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from logs.models import Logs
+
+
+class MailingLogListView(ListView):
+    model = Logs
+    context_object_name = 'mailing_logs'
+
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return Logs.objects.all()
+        queryset = Logs.objects.filter(mailing__user=self.request.user)
+        return queryset
